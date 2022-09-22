aaf0b57aa5cc7b5b843a0c0031f8181e53d83f80
abhijeet singh rajpurohit <abhijeetsinghbhainser@gmail.com>
2022-09-22 21:56:07 +0530

    search bar enabled

--- screens/Home.js
+++ screens/Home.js
@@ -6,17 +6,32 @@ import {COLORS, NFTData} from '../constants'
 import { FocusedStatusBar, HomeHeader, NFTCard } from '../components'
 
 const Home = ()=> {
+    const [nftData, setnftData] = useState(NFTData);
+
+    const handleSearch = (value) => {
+        if(!value.length) setnftData(NFTData)
+
+        const filteredData = NFTData.filter((item)=> 
+        item.name.toLowerCase().includes(value.toLowerCase())
+        );
+        if(filteredData.length){
+            setnftData(filteredData);
+        }else{
+            setnftData(NFTData);
+        }
+    }
+
     return (
         <SafeAreaView style={{flex: 1}}>
             <FocusedStatusBar background={COLORS.primary}/>
 
             <View style={{flex: 1}}>
                 <View style={{zIndex: 0}}>
-                    <FlatList data={NFTData} 
+                    <FlatList data={nftData} 
                         renderItem={({item})=> <NFTCard data={item} />} 
                         keyExtractor={(item)=> item.id} 
                         showsHorizontalScrollIndicator={false} 
-                        ListHeaderComponent={<HomeHeader />} 
+                        ListHeaderComponent={<HomeHeader onSearch={handleSearch}/>} 
                     />
                 </View>
                 <View style={{position: 'absolute', top: 0, bottom: 0, right: 0, left: 0, zIndex: -1}}>
